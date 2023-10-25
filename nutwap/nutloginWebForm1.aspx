d772fb8bdadcb6fa41130eca92ed9b015d02466f
name125asd <name125asd@gmail.com>
2023-10-25 17:47:07 +0700

    first commit

---
+++ nutwap/nutloginWebForm1.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" MasterPageFile="~/Site.Master" CodeBehind="nutloginWebForm1.aspx.cs" Inherits="nutwap.nutloginWebForm1" %>
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="jumbotron text-center">
+        <h1>Login</h1>
+        <p class="lead">Enter your username and password</p>
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <asp:TextBox ID="username" runat="server" CssClass="form-control mx-auto" placeholder="Username" Style="margin: auto;"></asp:TextBox>
+                <asp:TextBox ID="password" runat="server" TextMode="Password" CssClass="form-control mx-auto" placeholder="Password" Style="margin: auto;"></asp:TextBox>
+                <asp:Button ID="btnClickMe" runat="server" Text="Login" OnClick="btnClickMe_Click" CssClass="btn btn-primary" />
+            </div>
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <asp:Label ID="name" runat="server" CssClass="label-style"></asp:Label>
+            <asp:Label ID="address" runat="server" CssClass="label-style"></asp:Label>
+            <asp:Label ID="age" runat="server" CssClass="label-style"></asp:Label>
+            <asp:Label ID="email" runat="server" CssClass="label-style"></asp:Label>
+            <asp:Label ID="is_student" runat="server" CssClass="label-style"></asp:Label>
+        </div>
+    </div>
+</asp:Content>
---
+++ nutwap/nutloginWebForm1.aspx.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Newtonsoft.Json;
+
+namespace nutwap
+{
+    public partial class nutloginWebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void btnClickMe_Click(object sender, EventArgs e)
+        {
+            string getUser = username.Text;
+            string getPass = password.Text;
+            string dataReturn = apitest(getUser, getPass);
+            Response myObject = JsonConvert.DeserializeObject<Response>(dataReturn);
+
+            name.Text = myObject.name;
+            age.Text = myObject.age.ToString();
+            email.Text = myObject.email;
+            string test = myObject.address.ToString();
+            address.Text = JsonConvert.SerializeObject(myObject.address);
+
+
+
+
+
+        }
+        public string apitest(string user, string pass)
+        {
+            
+
+            string data = "";
+
+
+            var requestData = new
+            {
+                user = user,
+                pass = pass,
+
+            };
+
+            using (HttpClient client = new HttpClient())
+            {
+                string apiUrl = "https://localhost:44392/api/User"; // เปลี่ยน URL ของ API ตามที่คุณต้องการ
+
+                // สร้าง JSON จากข้อมูล requestData
+                string jsonData = JsonConvert.SerializeObject(requestData);
+                HttpContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = client.PostAsync(apiUrl, content).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                     data = response.Content.ReadAsStringAsync().Result;
+                    // ทำงานกับ JSON ที่ได้รับจาก API
+                }
+                else
+                {
+                    // การดำเนินการเมื่อร้อยอาร์เควสต์ไม่สำเร็จ
+                }
+            } // HttpClient จะถูกปลดล็อกที่นี่และทรัพยากรถูกปลดล็อกโดยอัตโนมัติ
+
+            return data;
+
+        }
+
+
+    }
+    public class Address
+    {
+        public string street { get; set; }
+        public string city { get; set; }
+        public string postal_code { get; set; }
+    }
+
+    public class Response
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+        public Address address { get; set; }
+        public string email { get; set; }
+        public bool is_student { get; set; }
+    }
+
+}
